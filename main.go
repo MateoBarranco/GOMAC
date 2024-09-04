5b2cf89a18518df079877f158bd94dde3e67a532
MateoBarranco <matebarranco@gmail.com>
2024-09-04 20:02:49 -0300

    primer commit

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "GOMAC/variables"
+
+func main() {
+
+	variables.Muestrvaiables()
+}
---
+++ variables/enteras.go
@@ -0,0 +1,16 @@
+package variables
+
+import (
+	"fmt"
+)
+
+func Muestrvaiables() {
+	var entero int
+	intde32 := int32(14)
+	intde64 := int64(28)
+
+	fmt.Println("los valores de la funcion son")
+	fmt.Println("entero =", entero)
+	fmt.Println("intde32 =", intde32)
+	fmt.Println("intde64 =", intde64)
+}
